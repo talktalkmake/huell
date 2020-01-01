10de7f1306f3e3ad26d83d09c75a882f5e7bfdcf
Tom Hart <kitsonian@gmail.com>
2020-01-01 14:03:04 -0500

    Removing logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h3 className="App">Huell</h3>
+      <p>What is Huell and what does it do?</p>
+      <ol>
+        <li>User registration</li>
+        <li>User login</li>
+        <li>User logout</li>
+        <li>User deletion</li>
+        <li>Pick colors</li>
+        <li>Add them to collections</li>
+      </ol>
+    </React.Fragment>
   );
 }
 
