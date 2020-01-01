a6dcba35cabb55afaab8718eafb9b4c457f82b82
Tom Hart <kitsonian@gmail.com>
2020-01-01 16:26:24 -0500

    configuring color picker and layout

--- src/App.js
+++ src/App.js
@@ -4,28 +4,38 @@ import "./App.css";
 import { HuePicker as Huell } from "react-color";
 
 function App() {
-  const [hue, setHue] = useState("ff0 17%");
+  const [hue, setHue] = useState("#0075ff");
 
   return (
     <React.Fragment>
-      <h3 className="App">Huell</h3>
-      <p>What is Huell and what does it do?</p>
-      <ol>
-        <li>User registration</li>
-        <li>User login</li>
-        <li>User logout</li>
-        <li>User deletion</li>
-        <li>Pick colors</li>
-        <li>Add them to collections</li>
-        <li>{hue}</li>
-      </ol>
-      <div className="hue-hold">
-        <Huell
-          direction="vertical"
-          width="16px"
-          height="200px"
-          onChangeComplete={color => setHue(color.hex)}
-        />
+      <div className="g">
+        <div className="h">
+          <h3 className="App">Huell</h3>
+          <p>What is Huell and what does it do?</p>
+          <ol>
+            <li>User registration</li>
+            <li>User login</li>
+            <li>User logout</li>
+            <li>User deletion</li>
+            <li>Pick colors</li>
+            <li>Add them to collections</li>
+            <li>{hue}</li>
+          </ol>
+        </div>
+        <div className="h">
+          <div className="hue-hold">
+            <Huell
+              color={hue}
+              direction="vertical"
+              width="16px"
+              height="200px"
+              onChangeComplete={color => setHue(color.hex)}
+            />
+          </div>
+          <div className="huell" style={{ backgroundColor: hue }}>
+            <p>{hue}</p>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
