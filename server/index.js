4430655f3e53ddd3cd9f40c3f4fcf2c3f0656b5d
Liz Rodriguez <liz@lizrodriguez.io>
2020-01-13 16:01:55 -0500

    add bodyparser and model folder

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+// json body parser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+)
+app.use(bodyParser.json())
+
+// Database Config
+// const db = pgp(process.env.DATABASE_URL)
 
 // send "hello" onto the webpage when you visit localhost:3001/hello
-
 app.get('/hello', function(req, res){
   res.send('hello');
 });
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+app.listen(process.env.PORT || 3001, () =>
+  console.log('Express server is running on localhost:3001 ┬──┬◡ﾉ(°-°ﾉ)')
 );
